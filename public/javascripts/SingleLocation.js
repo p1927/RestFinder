c48cc45cc0ac285479c095c593070544db9e0c71
Pratyush <99pratyush@gmail.com>
2018-03-03 15:35:00 +0530

    geolocation

--- public/javascripts/SingleLocation.js
+++ public/javascripts/SingleLocation.js
@@ -1,5 +1,46 @@
   $( document ).ready(function() {                             //first time hidehide confirm options
-        $(".confirm").find('.badge').toggle();  });
+        $(".confirm").find('.badge').toggle();
+
+$.getScript("http://maps.googleapis.com/maps/api/js?key=" + "AIzaSyAGX2KEn6RyrzyGAJ8KxxwUrTI4MeCMgtA", function()
+{
+    // script is now loaded and executed.
+    // put your dependent JS here.
+
+        if (navigator.geolocation) { //Checks if browser supports geolocation
+   navigator.geolocation.getCurrentPosition(function (position) {                                                              //This gets the
+     var latitude = position.coords.latitude;                    //users current
+     var longitude = position.coords.longitude;  
+     var accuracy = position.coords.accuracy;               //location
+     var coords = new google.maps.LatLng(latitude, longitude); //Creates variable for map coordinates
+     var directionsService = new google.maps.DirectionsService();
+     var directionsDisplay = new google.maps.DirectionsRenderer();
+     var mapOptions = //Sets map options
+     {
+       zoom: 15,  //Sets zoom level (0-21)
+       center: coords, //zoom in on users location
+       mapTypeControl: true, //allows you to select map type eg. map or satellite
+       navigationControlOptions:
+       {
+         style: google.maps.NavigationControlStyle.SMALL //sets map controls size eg. zoom
+       },
+       mapTypeId: google.maps.MapTypeId.ROADMAP //sets type of map Options:ROADMAP, SATELLITE, HYBRID, TERRIAN
+     };
+     map = new google.maps.Map( /*creates Map variable*/ document.getElementById("frame"), mapOptions /*Creates a new map using the passed optional parameters in the mapOptions parameter.*/);
+     directionsDisplay.setMap(map);
+     directionsDisplay.setPanel(document.getElementById('frame'));
+     var request = {
+       origin: coords,
+       destination: 'Barbeque Nation, Nanakramaguda Road, Beside Wipro Lake, Financial District, Gachibowli, Hyderabad',
+       travelMode: google.maps.DirectionsTravelMode.DRIVING   };
+
+       directionsService.route(request, function (response, status) {
+       if (status == google.maps.DirectionsStatus.OK) {
+         directionsDisplay.setDirections(response);  }       });
+                    
+    },function error(msg){ alert('Please enable your GPS position future.'); },{maximumAge:600000, timeout:5000, enableHighAccuracy: true} ); 
+ }
+   });
+   });
      
      $(".confirm").click(function () {                             //show confirm options
         $(this).find('.badge').toggle();       });
@@ -7,6 +48,7 @@
      $(".cancel").click(()=>{                              //hide confirm options
         $(this).find('.badge').toggle();        });
 
+     
      $("[id^=delete]").click(function(){                    //select  delete review icon span
      var id=this.id.split("delete")[1];
      var fullURL=$(location).attr('href').split('#')[0];
